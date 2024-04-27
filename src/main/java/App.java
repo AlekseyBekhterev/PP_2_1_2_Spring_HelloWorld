e909b9f55c5cb2834645f02293cbe4b5ec938784
AlekseyBekhterev <beh45@mail.ru>
2024-04-27 16:05:45 +0300

    решил задачу

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,14 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+
+        HelloWorld bean = (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat catBean1 = applicationContext.getBean(Cat.class);
+        Cat catBean2 = applicationContext.getBean(Cat.class);
+
+        System.out.println("сравнение BeanCat: - " + (catBean1 == catBean2));
+        System.out.println("сравнение BeanHelloworld: - " + (bean == bean2));
     }
 }
